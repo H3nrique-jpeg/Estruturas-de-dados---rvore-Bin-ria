514c3689067186558eea07781b109db1744aa247
Henrique Silva <cc25326@g.unicamp.br>
2026-09-10 18:11:36 -0300

    Implement binary search tree class in Java

---
+++ arvoreBinaria.java
@@ -0,0 +1,189 @@
+import java.lang.reflect.*;
+
+
+import javax.swing.plaf.TreeUI;
+
+
+public class ArvoreBinariaDeBusca <X extends Comparable<X>>
+{
+    private class No
+    {
+        private No esq;
+        private X  info;
+        private No dir;
+
+
+        public No (No e, X i, No d)
+        {
+            this.esq  = e;
+            this.info = i;
+            this.dir  = d;
+        }
+
+
+        public No (X i)
+        {
+            this.esq  = null;
+            this.info = i;
+            this.dir  = null;
+        }
+
+
+        public No getEsq ()
+        {
+            return this.esq;
+        }
+
+
+        public X getInfo ()
+        {
+            return this.info;
+        }
+
+
+        public No getDir ()
+        {
+            return this.dir;
+        }
+
+
+        public void setEsq (No e)
+        {
+            this.esq = e;
+        }
+       
+        public void setInfo (X i)
+        {
+            this.info = i;
+        }
+
+
+        public void setDir (No d)
+        {
+            this.dir = d;
+        }
+    } //fim da classe No
+
+
+    private No raiz;
+   
+    private X meuCloneDeX (X x)
+    {
+        X ret=null;
+
+
+        try
+        {
+            Class<?> classe         = x.getClass();
+            Class<?>[] tipoDosParms = null;
+            Method metodo           = classe.getMethod("clone",tipoDosParms);
+            Object[] parms          = null;
+            ret                     = (X)metodo.invoke(x,parms);
+        }
+        catch(NoSuchMethodException erro)
+        {}
+        catch(IllegalAccessException erro)
+        {}
+        catch(InvocationTargetException erro)
+        {}
+
+
+        return ret;
+    }
+   
+    public void guardeUmItem (X i) throws Exception
+    {
+        if (i==null) throw new Exception ("Informacao ausente");
+
+
+        if (i instanceof Cloneable) i=this.meuCloneDeX(i);
+       
+        if (this.raiz==null)
+        {
+            this.raiz = new No(i);
+            return;
+        }
+       
+        No atual=this.raiz;
+        for(;;) // forever
+        {
+            int comparacao=i.compareTo(atual.getInfo());
+           
+            if (comparacao==0) throw new Exception ("Elemento repetido");
+           
+            if (comparacao<0)
+            {
+                if (atual.getEsq()==null)
+                {
+                    atual.setEsq (new No (i));
+                    return;
+                }
+                else
+                    atual=atual.getEsq();
+            }
+            else // comparacao>0
+            {
+                if (atual.getDir()==null)
+                {
+                    atual.setDir (new No (i));
+                    return;
+                }
+                else
+                    atual=atual.getDir();
+            }
+        }
+    }
+   
+    public boolean temOItem (X i) throws Exception
+    {
+       if (i == null) throw new Exception("Item inexistente");
+       if (i instanceof Cloneable ) i = this.meuCloneDeX(i);
+   
+        if(this.raiz == null) return false;
+
+
+        No atual = this.raiz;
+        while(True){
+            int comparacao = i.compareTo(atual.getInfo());
+
+
+            if(comparacao == 0) return True;
+           
+        }
+
+
+     
+    }
+   
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (obj==this) return true;
+        if (obj==null) return false;
+        if (obj.getClass()!=this.getClass()) return false;
+        ArvoreBinariaDeBusca arv = (ArvoreBinariaDeBusca)obj;
+        return equals(this.raiz,arv.raiz);
+    }
+   
+    private boolean equals (No raizArv1, No raizArv2)
+    {
+        if (raizArv1==null && raizArv2==null) return true;
+        if (raizArv1==null && raizArv2!=null) return false;
+        if (raizArv1!=null && raizArv2==null) return false;
+        if (!raizArv1.getInfo().equals(raizArv2.getInfo())) return false;
+        /*
+        return equals(raizArv1.getEsq(),raizArv2.getEsq()) &&
+               equals(raizArv1.getDir(),raizArv2.getDir());
+        */
+        if (!equals(raizArv1.getEsq(),raizArv2.getEsq())) return false;
+        if (!equals(raizArv1.getDir(),raizArv2.getDir())) return false;
+        return true;
+    }
+   
+    // faça os demais métodos obrigatórios incluindo os "implements" apropriados
+} // fim da classe ArvoreBinariaDeBusca
+
+
+
+
+
